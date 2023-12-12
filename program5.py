15be8cbe62e13b307b9275bbf8ad997e83c2e4ba
AshishN40 <153530974+AshishN40@users.noreply.github.com>
2023-12-12 14:09:31 +0530

    Update program5.py

--- program5.py
+++ program5.py
@@ -1,1 +1,37 @@
 """Q5) Write a program to implement Breadth First Search Traversal"""
+from collections import deque
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+
+        while queue:
+            node = queue.popleft()
+            if node not in visited:
+                print(node, end=" ")
+                visited.add(node)
+                queue.extend(neighbor for neighbor in self.graph.get(node, []) if neighbor not in visited)
+
+# Example usage:
+if __name__ == "__main__":
+    # Create a sample graph
+    g = Graph()
+    g.add_edge(1, 2)
+    g.add_edge(1, 3)
+    g.add_edge(2, 4)
+    g.add_edge(2, 5)
+    g.add_edge(3, 6)
+    g.add_edge(3, 7)
+
+    # Perform BFS starting from node 1
+    print("BFS starting from node 1:")
+    g.bfs(1)
